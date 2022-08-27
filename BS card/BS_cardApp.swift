219b45104fd059da27915b560e3ac1f415cd060f
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2022-08-27 19:13:14 +0300

    Initial Commit

---
+++ BS card/BS_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BS_cardApp.swift
+//  BS card
+//
+//  Created by Oleksandr Smakhtin on 27.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BS_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
